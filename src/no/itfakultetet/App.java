10f3098dacb4c4655d89b88873f64698aa9e94a7
terjebh <terje@itfakultetet.no>
2022-04-18 15:06:54 +0200

    La til FileWriter

--- src/no/itfakultetet/App.java
+++ src/no/itfakultetet/App.java
@@ -13,13 +13,20 @@ public class App {
         while(true) {
             System.out.println("MASKINPARK");
             System.out.println("-".repeat(30));
-            System.out.println("1. Legg til ny maskin");
+            System.out.println("1. Legg til ny Desktop PC");
+            System.out.println("2. Legg til ny Laptop");
             System.out.println("3. Vis maskiner");
-            System.out.println("q. Avslutt3");
+            System.out.println("q. Avslutt");
             menyvalg = in.nextLine();
             if(menyvalg.equals("q")) {
                 System.out.println("Bye....");
                 exit (0);
+            } else if (menyvalg.equals("1")) {
+            // Legg til ny Desktop PC
+
+            } else if (menyvalg.equals("2")) {
+            //Legg til ny aptop
+            Laptop.addNew();
             }
         }
 
--- src/no/itfakultetet/Laptop.java
+++ src/no/itfakultetet/Laptop.java
@@ -1,17 +1,61 @@
 package no.itfakultetet;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Laptop extends Maskin {
-static int antallLaptop;
+    static int antallLaptop;
     private int ram;
     private int antallDisker, batteritid;
     private float skjermstørrelse;
+
+    public Laptop() {
+    }
 
     public Laptop(String type, String merke, int ram, int antallDisker, int pris, int modell, LocalDate innkjøpt) {
         super(type, merke, ram, antallDisker, pris, modell, innkjøpt);
         Maskin.antallMaskiner++;
         antallLaptop++;
+    }
+
+    public static void addNew() {
+        Laptop nyLaptop = new Laptop();
+        Scanner lapIn = new Scanner(System.in);
+        System.out.println("Ny Laptop:");
+        System.out.println("Type: ");
+        nyLaptop.setType(lapIn.nextLine());
+        System.out.println("Merke: ");
+        nyLaptop.setMerke(lapIn.nextLine());
+        System.out.println("Pris: ");
+        nyLaptop.setPris(Integer.parseInt(lapIn.nextLine()));
+        System.out.println("GB Ram: ");
+        nyLaptop.setRam(Integer.parseInt(lapIn.nextLine()));
+        System.out.println("Skjermstørrelse i tommer: ");
+        nyLaptop.setSkjermstørrelse(Float.parseFloat(lapIn.nextLine()));
+        System.out.println("Innkjøpt: ");
+        nyLaptop.setInnkjøpt(LocalDate.parse(lapIn.nextLine()));
+
+        lagre(nyLaptop);
+
+    }
+
+    private static void lagre(Laptop nyLaptop) {
+        try (FileWriter fil = new FileWriter("data.csv",true)) {
+            fil.append(nyLaptop.getType() + "," + nyLaptop.getMerke() + "," + nyLaptop.getPris() + ","
+                    + nyLaptop.getRam() + "," + nyLaptop.getSkjermstørrelse() + "," + nyLaptop.getInnkjøpt()+"\n");
+            fil.flush();
+            fil.close();
+            System.out.println("Laptop lagret...");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public int getBatteritid() {
